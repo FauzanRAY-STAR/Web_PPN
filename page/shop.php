ac918b4dd445ecb569a3b28fe0c3e805f1da6445
Gibranrhmn <ganeshagibran4@gmail.com>
2025-11-11 14:30:30 +0700

    fixing shop dropdown stuck

--- page/shop.php
+++ page/shop.php
@@ -6,6 +6,9 @@
     <title>Toko Online - Belanja Sekarang</title>
     <link rel="stylesheet" href="../asset/style/shop.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
 </head>
 <body>
     <!-- Navbar -->
